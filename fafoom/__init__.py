3b5ea43c640184429b5180d5fbde8c2af45ab3fa
kimgreis <50487912+kimgreis@users.noreply.github.com>
2019-05-10 14:29:20 +0200

    Add files via upload
    A little edit to make Gaussian work and make RDkit work again.

--- fafoom/__init__.py
+++ fafoom/__init__.py
@@ -1,9 +1,10 @@
 from structure import MoleculeDescription, Structure
 from genetic_operations import selection, crossover_single_point, crossover_random_points
 from pyaims import AimsObject
-#from pyff import FFObject
+from pyff import FFObject
 from pynwchem import NWChemObject
 from pyorca import OrcaObject
+from pygaussian import GaussianObject
 from pyforcefield import FFobject
 from utilities import *
 from run_utilities import *
